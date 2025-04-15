18930ebca917832b37e7b6341eb0338aabf55249
adamolofsson <adamolofssson@gmail.com>
2025-04-15 18:39:27 +0200

    Update Arduino-PathAware.ino

--- Arduino-PathAware/Arduino-PathAware.ino
+++ Arduino-PathAware/Arduino-PathAware.ino
@@ -1,9 +1,28 @@
+int ruta0;
+int ruta1;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello World");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ruta0 = analogRead(A0);
+  ruta1 = analogRead(A1);
 
+  if (ruta0 >= 0) {
+    Serial.print("Ruta0 aktiverad, värde ");
+    Serial.println(ruta0);
+  }
+  else {
+    Serial.println("Ruta0 inaktiv");
+  }
+
+  if (ruta1 >= 0) {
+    Serial.print("Ruta1 aktiverad, värde ");
+    Serial.println(ruta1);
+  }
+  else {
+    Serial.println("Ruta1 inaktiv");
+  }
+  delay(1000);
 }
